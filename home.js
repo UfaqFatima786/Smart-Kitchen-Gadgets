4cc008981e8752f6fa4d147f57e26260de9b0f80
UfaqFatima786 <u.capricorn19@gmail.com>
2026-05-04 13:27:56 +0500

    gadgets

---
+++ home.js
@@ -0,0 +1,11 @@
+function products (a,b,c){
+    this.name=a;
+    this.price=b;
+    this.src=c;
+}
+
+var treandingProducts = [
+    new products("Dinner Set", "1500 EGP", "images/dinner set.jpg"),
+    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
+        new products("Glass Set", "500 EGP", "images/glass set.jpg"),
+]
