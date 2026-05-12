fcb5af23dcf1f2c7e42db9f6cdc623b72aced204
UfaqFatima786 <u.capricorn19@gmail.com>
2026-05-13 02:09:50 +0500

    gadgets

--- home.js
+++ home.js
@@ -19,3 +19,24 @@ var newArrivals = [
     new products("Glass Set", "500 EGP", "images/glass set.jpg"),
     new products("Glass Set", "500 EGP", "images/glass set.jpg"),
 ]
+
+products.prototype.renderCard = function () {
+    return `
+
+     <div class="col">
+    <div class="card h-100">
+      <img src="${this.src}" class="card-img-top gadgets-img" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${this.name.toUpperCase()}</h5>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Price:${this.price}</small>
+ <button onclick='addToCart(${JSON.stringify(this)})'
+        class="bg-black text-white px-2 py-2 rounded border-0">
+        Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>
+          `
+};
