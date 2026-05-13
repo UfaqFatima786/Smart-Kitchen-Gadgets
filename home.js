770a09febced9246d5160a49793b1882aa129cb2
UfaqFatima786 <u.capricorn19@gmail.com>
2026-05-13 11:27:46 +0500

    kitchen gagdets

--- home.js
+++ home.js
@@ -5,19 +5,19 @@ function products (a,b,c){
 }
 
 var treandingProducts = [
-    new products("Dinner Set", "1500 EGP", "images/dinner set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
+    new products("Dinner Set", "15000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2026/03/Porcelain-Buffet-Dish-Food-Warmer-Set.jpg?resize=247%2C247&ssl=1"),
+    new products("dinner Set", "20000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2026/01/Winsor-England-Heritage-Black-Porcelain-Dinner-Set-1.jpg?resize=400%2C400&ssl=1"),
+    new products("glass set", "1500 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2026/04/Crystal-Lining-Glass-Set-of-6-Golden-Line-1.jpg?resize=400%2C400&ssl=1"),
 ]
 var bestSellers = [
-    new products("Dinner Set", "1500 EGP", "images/dinner set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
+    new products("WINSOR ENGLAND HERITAGE BLACK PORCELAIN DINNER SET", "125000 PKR", "https://homegadgets.pk/wp-content/uploads/2025/10/Fryers-and-Pans-in-Pakistan-400x400.jpg"),
+    new products("freypan set Set", "20000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2026/04/Baroly-Earth-Grey-Frypan-24cm.jpg?resize=247%2C247&ssl=1"),
+    new products("black dish  set", "15000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2026/04/Baroly-Kappe-Deep-Fryer-24cm-with-Strainer.jpg?resize=247%2C247&ssl=1"),
 ]
 var newArrivals = [
-    new products("Dinner Set", "1500 EGP", "images/dinner set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
-    new products("Glass Set", "500 EGP", "images/glass set.jpg"),
+    new products("grey utensil Set", "10000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2025/10/Cooking-Utensils-in-Pakistan.jpg?resize=247%2C247&ssl=1"),
+    new products("khife and spoon Set", "2000 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2025/10/2-Piece-Spoon-Organizer-with-Metal-Rack-Kitchen-Spoon-Holder-3.jpg?resize=247%2C247&ssl=1"),
+    new products("vlack utensil Set", "1500 PKR", "https://i0.wp.com/homegadgets.pk/wp-content/uploads/2025/10/Nonstick-Kitchen-Cooking-Utensils-7Pcs-Set.jpg?resize=247%2C247&ssl=1"),
 ]
 
 products.prototype.renderCard = function () {
@@ -40,3 +40,14 @@ products.prototype.renderCard = function () {
   </div>
           `
 };
+
+var ourItems = document.getElementById("ourproducts");
+for (var item of treandingProducts) {
+    ourItems.innerHTML += item.renderCard();
+}
+for (var item of bestSellers) {
+    ourItems.innerHTML += item.renderCard();
+}
+for (var item of newArrivals) {
+    ourItems.innerHTML += item.renderCard();
+}
